99dc874cf0ee73b808d20069c8a0b07af3846c76
Kotaro Kamashima <kotaro0726@gmail.com>
2021-06-28 08:46:50 +0900

    Update: Create directory by git init

--- srcs/my_init/init.go
+++ srcs/my_init/init.go
@@ -1,7 +1,23 @@
 package my_init
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func Init() {
-	fmt.Println("Initialized empty Git repository in /Users/kotarokamashima/Desktop/local/.git/")
+	currentDir, _ := os.Getwd()
+	if err := os.Mkdir(".my_git", 0777); err != nil {
+		fmt.Println(err)
+	}
+	if err := os.MkdirAll(".my_git/index", 0777); err != nil {
+		fmt.Println(err)
+	}
+	if err := os.MkdirAll(".my_git/refs/main", 0777); err != nil {
+		fmt.Println(err)
+	}
+	if err := os.MkdirAll(".my_git/objects", 0777); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Initialized empty Git repository in %s/.my_git/\n", currentDir)
 }
