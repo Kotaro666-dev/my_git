b1d7c8ebd5fd58cef75c990eb48cbde7738f4162
Kotaro Kamashima <kotaro0726@gmail.com>
2021-06-27 21:58:26 +0900

    Update: Add part to read command line args and stdout by arg case

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	length := len(os.Args)
+	if length == 1 {
+		fmt.Println("git help")
+		os.Exit(1)
+	}
+	arg := os.Args[1]
+	switch arg {
+		case "add":
+			fmt.Println("my_git add")
+		case "commit":
+			fmt.Println("my_git commit")
+		default:
+			fmt.Printf("my_git: '%s' is not a git command. See 'git --help'.\n", arg)
+			os.Exit(1)
+	}
 }
