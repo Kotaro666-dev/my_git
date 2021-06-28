f2bada010f796058b659adf78c76b166203c18b3
Kotaro Kamashima <kotaro0726@gmail.com>
2021-06-28 22:09:32 +0900

    Update: add writeIndex func

--- srcs/add/add.go
+++ srcs/add/add.go
@@ -1,9 +1,37 @@
 package add
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+//func doesFileExist(name string) bool {
+//	if _, err := os.Stat(name); err != nil {
+//		if os.IsNotExist(err) {
+//			return false
+//		}
+//	}
+//	return true
+//}
+
+func writeIndex() {
+	indexPath := ".my_git/index"
+	file, err := os.OpenFile(indexPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := file.Write([]byte("test\n")); err != nil {
+		log.Fatal(err)
+	}
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func Add(args []string) {
 	fmt.Println("my_git add")
+	writeIndex()
 	var i int = 2
 	for i < len(args) {
 		//fmt.Printf("args[%d] -> %s\n", i, args[i])
